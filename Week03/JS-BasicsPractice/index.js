2e23027b6b76e0259cd7c75b054683afaf4411e7
Nadira Ali <nadiraali188a@gmail.com>
2021-12-27 11:02:18 +0000

    week 3 initial commit

---
+++ Week03/JS-BasicsPractice/index.js
@@ -0,0 +1,38 @@
+console.log ('Hello World');
+
+let name = ('Nadira');
+let age = (28)
+let genderType = ('Female')
+console.log (name);
+console.log (age);
+console.log (genderType);
+
+let hobby = ('binging netflix')
+console.log (hobby)
+
+let person = {
+    name: 'Nadira',
+    age: 28,
+    genderType: 'female',
+    hobby: 'binging netflix'
+};
+console.log (person)
+
+let favoriteColours = ['green', 'white'];
+favoriteColours[2] = 'yellow';
+favoriteColours[3] = 'black'
+console.log (favoriteColours)
+
+function greet(gender) {
+    console.log ('Hello' + " " + gender)
+}
+greet('ladies')
+greet('gentlemen')
+
+function square (number) {
+    return number * number;
+}
+console.log (square(2));
+console.log (square(4));
+console.log (square(6));
+console.log (square(8));
