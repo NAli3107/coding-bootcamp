5a1cbba8ba25e26a07983fadef143de766fbeb3e
Nadira Ali <nadiraali188a@gmail.com>
2021-12-27 12:29:57 +0000

    stu activity 4 complete

---
+++ Week03/JS-HelloVariantActivity/script.js
@@ -0,0 +1,16 @@
+// Write Your JavaScript Code Here
+personName = 'Nadira'
+pets = '0'
+funFact = 'I speak 3 languages'
+
+console.log('My name is' + " " + personName)
+console.log('I have' + " " + pets + " " + 'pets')
+console.log('Fun fact:' + " " + funFact)
+
+personName = 'Sumaya'
+pets = '10'
+funFact = 'Lived in several countries'
+
+console.log('My anme is' + " " + personName)
+console.log('I have' + " " + pets + " " + 'pets')
+console.log('Fun fact:' + " " + funFact)
