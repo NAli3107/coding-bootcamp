2ff6f6c052b742eaccf47dc73db96e0439c89471
Nadira Ali <nadiraali188a@gmail.com>
2021-12-27 12:37:10 +0000

    stu activity 4 let keyword added

--- Week03/JS-HelloVariantActivity/script.js
+++ Week03/JS-HelloVariantActivity/script.js
@@ -1,15 +1,15 @@
 // Write Your JavaScript Code Here
-personName = 'Nadira'
-pets = '0'
-funFact = 'I speak 3 languages'
+let personName = 'Nadira'
+let pets = '0'
+let funFact = 'I speak 3 languages'
 
 console.log('My name is' + " " + personName)
 console.log('I have' + " " + pets + " " + 'pets')
 console.log('Fun fact:' + " " + funFact)
 
-personName = 'Sumaya'
-pets = '10'
-funFact = 'Lived in several countries'
+let personName = 'Sumaya'
+let pets = '10'
+let funFact = 'Lived in several countries'
 
 console.log('My anme is' + " " + personName)
 console.log('I have' + " " + pets + " " + 'pets')
