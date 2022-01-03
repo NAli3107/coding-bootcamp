dc45153eeb33ff241dd00a87b20ea0cb7cc93e4f
Nadira Ali <nadiraali188a@gmail.com>
2022-01-03 13:25:33 +0000

    stu activity 24 complete

--- Week03/JS-ObjectsActivity/script.js
+++ Week03/JS-ObjectsActivity/script.js
@@ -3,14 +3,14 @@
 const customerOrder = {
     drinkName:"coffee", 
     sugar: 2, 
-    orderStatus: true,
+    orderReady: true,
     
 };
 
 console.log("Your order is " + customerOrder.drinkName + " with " + customerOrder.sugar + " sugars.")
 
 
-if (customerOrder.orderStatus) {
+if (customerOrder.orderReady) {
     console.log("Ready for pick-up")
 } else {
     console.log("Still in order queue")
